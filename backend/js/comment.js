2bc9d358b48c8076fb306a19c03327a8d43c6f97
NicxsCodeSymphony <nicxsician@gmail.com>
2024-06-02 22:31:44 +0800

    only current user

--- backend/js/comment.js
+++ backend/js/comment.js
@@ -1,134 +1,154 @@
 $(document).ready(function() {
     var editCommentID;
+    var currentUserId = null;
 
-    // Event binding for comment button
-    $(document).on('click', '#comment-btn', function(e) {
-        e.preventDefault();
-        var urlParams = new URLSearchParams(window.location.search);
-        var id = urlParams.get('id');
-        var comment = $('.commentInput').val();
-
-
-        $.ajax({
-            url: '../backend/php/comment.php',
-            method: 'POST',
-            data: {
-                addComment: true,
-                post_id: id,
-                comment: comment,
-            },
-            dataType: 'json',
-            success: function(response) {
-                alert("COMMENT ADDED SUCCESSFULLY");
-                window.location.reload();
-            },
-            error: function(xhr, status, error) {
-                console.error("An error occurred:", error);
-                alert("Failed to add comment. Please try again.");
+    $.ajax({
+        url: '../backend/php/session.php',
+        method: 'GET',
+        dataType: 'json',
+        success: function(response) {
+            if (response.user_id) {
+                currentUserId = response.user_id;
+                loadComments();
+            } else {
+                alert('User not logged in');
             }
+        },
+        error: function(xhr, status, error) {
+            console.error("AJAX request failed:", status, error);
+            alert("An error occurred while fetching the current user ID.");
+        }
+    });
+
+    function loadComments() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const postId = urlParams.get('id');
+
+        $('.comments-container').load('template.html #comment-template', function() {
+            var commentsDiv = $('#comment-template').html();
+            $.ajax({
+                url: '../backend/php/comment.php',
+                method: 'GET',
+                data: { id: postId },
+                dataType: 'json',
+                success: function(response) {
+                    console.log(response.comments);
+                    response.comments.forEach(function(comment) {
+                        var $comments = $(commentsDiv);
+                        $comments.find(".user_name").text('@' + comment.username);
+                        $comments.find(".friend_image").attr('src', '../backend/php/' + comment.friend_image);
+                        $comments.find(".comment-text").text(comment.comment);
+                        $comments.find('.Edit').attr('data-post-id', comment.id);
+                        $comments.find('.Delete').attr('data-post-id', comment.id);
+                        
+                        // Hide edit and delete buttons if comment user ID is not equal to current user ID
+                        if (comment.user_id !== currentUserId) {
+                            $comments.find('.Edit').hide();
+                            $comments.find('.Delete').hide();
+                        }
+
+                        $('.comments-container').append($comments);
+                    });
+                }
+            });
         });
-    });
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const postId = urlParams.get('id');
+        // Event binding for comment button
+        $(document).on('click', '#comment-btn', function(e) {
+            e.preventDefault();
+            var comment = $('.commentInput').val();
 
-    $('.comments-container').load('template.html #comment-template', function() {
-        var commentsDiv = $('#comment-template').html();
-        $.ajax({
-            url: '../backend/php/comment.php',
-            method: 'GET',
-            data: { id: postId },
-            dataType: 'json',
-            success: function(response) {
-                console.log(response.comments);
-                response.comments.forEach(function(comment) {
-                    var $comments = $(commentsDiv);
-                    $comments.find(".user_name").text('@' + comment.username);
-                    $comments.find(".friend_image").attr('src', '../backend/php/' + comment.friend_image);
-                    $comments.find(".comment-text").text(comment.comment);
-                    $comments.find('.Edit').attr('data-post-id', comment.id);
-                    $comments.find('.Delete').attr('data-post-id', comment.id);
-                    $('.comments-container').append($comments);
-                });
-            }
-        })
-    })
-
-    // Open edit modal
-    $('.comments-container').on('click', '.Edit', function() {
-        editCommentID = $(this).attr('data-post-id');
-        $.ajax({
-            url: '../backend/php/comment.php',
-            method: 'POST',
-            data: {
-                getComment: true,
-                comment_id: editCommentID,
-            },
-            dataType: 'json',
-            success: function(response) {
-                $('#commentText').val(response.comment);
-                $('#editModal').show();
-            },
-            error: function(xhr, status, error) {
-                console.error("AJAX request failed:", status, error);
-                alert("An error occurred while making the request.");
-            }
+            $.ajax({
+                url: '../backend/php/comment.php',
+                method: 'POST',
+                data: { 
+                    addComment: true,
+                    post_id: postId,
+                    comment: comment,
+                },
+                dataType: 'json',
+                success: function(response) {
+                    alert("COMMENT ADDED SUCCESSFULLY");
+                    window.location.reload();
+                },
+                error: function(xhr, status, error) {
+                    console.error("An error occurred:", error);
+                    alert("Failed to add comment. Please try again.");
+                }
+            });
         });
-    });
 
-    // Close modal
-    $('.modal .close').on('click', function() {
-        $('#editModal').hide();
-    });
-
-    // Save edited comment
-    $('.modal .close').on('click', function() {
-        $('#editModal').hide();
-    });
-
-    // Save edited comment
-    $('#saveComment').on('click', function() {
-        var updatedComment = $('#commentText').val();
-        $.ajax({
-            url: '../backend/php/comment.php',
-            method: 'POST',
-            data: {
-                updateComment: true,
-                comment_id: editCommentID,
-                comment: updatedComment
-            },
-            dataType: 'json',
-            success: function(response) {
-                alert("Comment updated successfully");
-                $('#editModal').hide();
-                window.location.reload();
-            },
-            error: function(xhr, status, error) {
-                console.error("AJAX request failed:", status, error);
-                alert("An error occurred while updating the comment.");
-            }
+        // Open edit modal
+        $('.comments-container').on('click', '.Edit', function() {
+            editCommentID = $(this).attr('data-post-id');
+            $.ajax({
+                url: '../backend/php/comment.php',
+                method: 'POST',
+                data: {
+                    getComment: true,
+                    comment_id: editCommentID,
+                },
+                dataType: 'json',
+                success: function(response) {
+                    $('#commentText').val(response.comment);
+                    $('#editModal').show();
+                },
+                error: function(xhr, status, error) {
+                    console.error("AJAX request failed:", status, error);
+                    alert("An error occurred while making the request.");
+                }
+            });
         });
-    });
 
-    // Delete comment
-    $('.comments-container').on('click', '.Delete', function() {
-        var commentID = $(this).attr('data-post-id');
-        $.ajax({
-            url: '../backend/php/comment.php',
-            method: 'POST',
-            data: {
-                deleteComment: true,
-                comment_id: commentID,
-            },
-            dataType: 'json',
-            success: function(response) {
-                alert("Successfully Deleted");
-                window.location.reload();
-            },
-            error: function(xhr, status, error) {
-                console.error("AJAX request failed:", status, error);
-                alert("An error occurred while making the request.");
-            }
+        // Close modal
+        $('.modal .close').on('click', function() {
+            $('#editModal').hide();
         });
-    });
+
+        // Save edited comment
+        $('#saveComment').on('click', function() {
+            var updatedComment = $('#commentText').val();
+            $.ajax({
+                url: '../backend/php/comment.php',
+                method: 'POST',
+                data: {
+                    updateComment: true,
+                    comment_id: editCommentID,
+                    comment: updatedComment
+                },
+                dataType: 'json',
+                success: function(response) {
+                    alert("Comment updated successfully");
+                    $('#editModal').hide();
+                    window.location.reload();
+                },
+                error: function(xhr, status, error) {
+                    console.error("AJAX request failed:", status, error);
+                    alert("An error occurred while updating the comment.");
+                }
+            });
+        });
+
+        // Delete comment
+        $('.comments-container').on('click', '.Delete', function() {
+            var commentID = $(this).attr('data-post-id');
+            $.ajax({
+                url: '../backend/php/comment.php',
+                method: 'POST',
+                data: {
+                    deleteComment: true,
+                    comment_id: commentID,
+                },
+                dataType: 'json',
+                success: function(response) {
+                    alert("Successfully Deleted");
+                    window.location.reload();
+                },
+                error: function(xhr, status, error) {
+                    console.error("AJAX request failed:", status, error);
+                    alert("An error occurred while making the request.");
+                }
+            });
+        });
+    }
 });
--- backend/js/customHooks.js
+++ backend/js/customHooks.js
@@ -1,15 +1,131 @@
 $(document).ready(function() {
     let post_id = "";
-    $('.screen').append($('<div>').load('footer.html'));
-    $('.friendList').load('template.html #friend-template', function() {
-        $('.post-container').load('template.html #post-template', function() {
-            var friendTemplate = $('#friend-template').html();
-            var postTemplate = $('#post-template').html();
+    let currentUserId = null;
+    
+    $.ajax({
+        url: '../backend/php/session.php',
+        method: 'GET',
+        dataType: 'json',
+        success: function(response) {
+            if (response.user_id) {
+                currentUserId = response.user_id;
+                loadContent();
+            } else {
+                alert('User not logged in');
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error("AJAX request failed:", status, error);
+            alert("An error occurred while fetching the current user ID.");
+        }
+    });
 
-            // for (var i = 0; i < 10; i++) {
-            //     $('.friendList').append(friendTemplate);
-            // }
+    function loadContent() {
+        $('.screen').append($('<div>').load('footer.html'));
+        $('.friendList').load('template.html #friend-template', function() {
+            $('.post-container').load('template.html #post-template', function() {
+                var friendTemplate = $('#friend-template').html();
+                var postTemplate = $('#post-template').html();
 
+                // for (var i = 0; i < 10; i++) {
+                //     $('.friendList').append(friendTemplate);
+                // }
+
+                $.ajax({
+                    url: '../backend/php/notifs.php',
+                    method: 'GET',
+                    dataType: 'json',
+                    success: function(response) {
+                        console.log(response); 
+                        if (response.status === 'success') {
+                            response.users.forEach(function(user) {
+                                console.log(user);
+                                var $friendList = $(friendTemplate); 
+                                $friendList.find('.friendAvatar').attr('src', "../backend/php/" + user.friend_image);
+                                $friendList.find('.friendUsername').text(user.friend_username);
+                                $('.friendList').append($friendList);
+                            });
+                        } else {
+                            alert(response.message);
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        console.error("AJAX request failed:", status, error);
+                        alert("An error occurred while making the request.");
+                    }
+                });
+                
+
+                $.ajax({
+                    url: "../backend/php/post.php",
+                    type: "GET",
+                    success: function(data){
+                        data.forEach(function(post) {
+                            var $post = $(postTemplate);
+                            $post.find('.editPost').attr('data-post-id', post.post_id);
+                            $post.find('.post-id').val(post.post_id);
+                            $post.find('#postUsername').text("@" + post.username);
+                            $post.find('.post-time').text(post.name);
+                            $post.find('#userImage').attr('src', "../backend/php/" + post.profile_photo);
+                            $post.find('.caption').text(post.caption);
+                            $post.find('.poster-avatar').attr('src', post.poster_avatar);
+                            $post.find('.post-image').attr('src', "../backend/php/" + post.image);
+
+                            // Hide the editPost element if the poster_id is not equal to the current user ID
+                            if (post.poster_id !== currentUserId) {
+                                $post.find('.editPost').hide();
+                            }
+
+                            $('.post-container').append($post);
+                        });
+                    },
+                    error: function(xhr, status, error) {
+                        console.error("An error occurred: " + error);
+                    }
+                });
+                
+            });
+            
+        });
+
+        let friendTemplate = '';
+        
+        // Load the friend template
+        $('.friend-list').load('template.html #add-friend-template', function() {
+            friendTemplate = $('#add-friend-template').html();
+
+            // Make the AJAX request
+            $.ajax({
+                url: '../backend/php/friend.php',
+                method: 'GET',
+                dataType: 'json',
+                success: function(response) {
+                    console.log(response); // Log the response to inspect it
+                    if (response.status === 'success') {
+                        response.users.forEach(function(user) {
+                            var $friend = $(friendTemplate);
+                            $friend.find('.friend-name').text(user.username);
+                            $friend.find('.user-id').val(user.id);
+                            $friend.find('.friend-image').attr('src', `../backend/php/${user.profile_photo}`);
+                            $('.friend-list').append($friend);
+                        });
+                    } else {
+                        // alert(response.message);
+                    }
+                },
+                error: function(xhr, status, error) {
+                    console.error("AJAX request failed:", status, error);
+                    alert("An error occurred while making the request.");
+                }
+            });
+            
+            
+        });
+
+
+        $('.notifs-container').load('template.html #notifs-template', function(){
+            var notification = $('#notifs-template').html();
+            // Make the AJAX request
             $.ajax({
                 url: '../backend/php/notifs.php',
                 method: 'GET',
@@ -19,10 +135,39 @@ $(document).ready(function() {
                     if (response.status === 'success') {
                         response.users.forEach(function(user) {
                             console.log(user);
-                            var $friendList = $(friendTemplate); 
-                            $friendList.find('.friendAvatar').attr('src', "../backend/php/" + user.friend_image);
-                            $friendList.find('.friendUsername').text(user.friend_username);
-                            $('.friendList').append($friendList);
+                            var $notifs = $(notification);
+                            $notifs.find('.user_name').text(user.friend_name);
+                            $notifs.find('.friend_image').attr('src', `../backend/php/${user.friend_image}`);
+        
+                            if(user.status === 'request'){
+                                const currentTime = new Date();
+                                const notificationTime = new Date(user.time_created);
+                                const diffMs = currentTime - notificationTime;
+                                const diffSec = Math.floor(diffMs / 1000);
+                                const diffMin = Math.floor(diffSec / 60);
+                                const diffHour = Math.floor(diffMin / 60);
+                                const diffDay = Math.floor(diffHour / 24);
+                                if (diffHour > 0) {
+                                    $notifs.find('.notif-text').text('sent you a friend request ' + (diffHour === 1 ? '1 hour ago' : diffHour + ' hours ago'));
+                                } else if (diffMin > 0) {
+                                    $notifs.find('.notif-text').text('sent you a friend request ' + (diffMin === 1 ? '1 minute ago' : diffMin + ' minutes ago'));
+                                } else if (diffSec > 0) {
+                                    $notifs.find('.notif-text').text('sent you a friend request ' + (diffSec === 1 ? '1 second ago' : diffSec + ' seconds ago'));
+                                } else {
+                                    $notifs.find('.notif-text').text('sent you a friend request ' + (diffDay === 1 ? '1 day ago' : diffDay + ' days ago'));
+                                }
+                                $notifs.find('.see').hide();
+                                $notifs.find('.accept').show();
+                                $notifs.find('.ignore').show();
+                            } else {
+                                // $notifs.find('.actions').hide();
+                            $notifs.find('.notif-text').text('You are now friends with @' + user.friend_username);
+                                $notifs.find('.accept').hide();
+                                $notifs.find('.ignore').hide();
+                            }
+                            
+                            $notifs.find('.friend_id').val(user.friend_id);
+                            $('.notifs-container').append($notifs);
                         });
                     } else {
                         alert(response.message);
@@ -33,126 +178,6 @@ $(document).ready(function() {
                     alert("An error occurred while making the request.");
                 }
             });
-            
-
-            $.ajax({
-                url: "../backend/php/post.php",
-                type: "GET",
-                success: function(data){
-                    data.forEach(function(post) {
-                        var $post = $(postTemplate);
-                        $post.find('.editPost').attr('data-post-id', post.post_id);
-                        $post.find('.post-id').val(post.post_id);
-                        $post.find('#postUsername').text("@" + post.username);
-                        $post.find('.post-time').text(post.name);
-                        $post.find('#userImage').attr('src', "../backend/php/" + post.profile_photo);
-                        $post.find('.caption').text(post.caption);
-                        $post.find('.poster-avatar').attr('src', post.poster_avatar);
-                        $post.find('.post-image').attr('src', "../backend/php/" + post.image);
-                        $('.post-container').append($post);
-                    });
-                },
-                error: function(xhr, status, error) {
-                    console.error("An error occurred: " + error);
-                }
-            });
-            
         });
-        
-    });
-
-    let friendTemplate = '';
-    
-    // Load the friend template
-    $('.friend-list').load('template.html #add-friend-template', function() {
-        friendTemplate = $('#add-friend-template').html();
-
-        // Make the AJAX request
-        $.ajax({
-            url: '../backend/php/friend.php',
-            method: 'GET',
-            dataType: 'json',
-            success: function(response) {
-                console.log(response); // Log the response to inspect it
-                if (response.status === 'success') {
-                    response.users.forEach(function(user) {
-                        var $friend = $(friendTemplate);
-                        $friend.find('.friend-name').text(user.username);
-                        $friend.find('.user-id').val(user.id);
-                        $friend.find('.friend-image').attr('src', `../backend/php/${user.profile_photo}`);
-                        $('.friend-list').append($friend);
-                    });
-                } else {
-                    // alert(response.message);
-                }
-            },
-            error: function(xhr, status, error) {
-                console.error("AJAX request failed:", status, error);
-                alert("An error occurred while making the request.");
-            }
-        });
-        
-        
-    });
-
-
-    $('.notifs-container').load('template.html #notifs-template', function(){
-        var notification = $('#notifs-template').html();
-        // Make the AJAX request
-        $.ajax({
-            url: '../backend/php/notifs.php',
-            method: 'GET',
-            dataType: 'json',
-            success: function(response) {
-                console.log(response); 
-                if (response.status === 'success') {
-                    response.users.forEach(function(user) {
-                        console.log(user);
-                        var $notifs = $(notification);
-                        $notifs.find('.user_name').text(user.friend_name);
-                        $notifs.find('.friend_image').attr('src', `../backend/php/${user.friend_image}`);
-    
-                        if(user.status === 'request'){
-                            const currentTime = new Date();
-                            const notificationTime = new Date(user.time_created);
-                            const diffMs = currentTime - notificationTime;
-                            const diffSec = Math.floor(diffMs / 1000);
-                            const diffMin = Math.floor(diffSec / 60);
-                            const diffHour = Math.floor(diffMin / 60);
-                            const diffDay = Math.floor(diffHour / 24);
-                            if (diffHour > 0) {
-                                $notifs.find('.notif-text').text('sent you a friend request ' + (diffHour === 1 ? '1 hour ago' : diffHour + ' hours ago'));
-                            } else if (diffMin > 0) {
-                                $notifs.find('.notif-text').text('sent you a friend request ' + (diffMin === 1 ? '1 minute ago' : diffMin + ' minutes ago'));
-                            } else if (diffSec > 0) {
-                                $notifs.find('.notif-text').text('sent you a friend request ' + (diffSec === 1 ? '1 second ago' : diffSec + ' seconds ago'));
-                            } else {
-                                $notifs.find('.notif-text').text('sent you a friend request ' + (diffDay === 1 ? '1 day ago' : diffDay + ' days ago'));
-                            }
-                            $notifs.find('.see').hide();
-                            $notifs.find('.accept').show();
-                            $notifs.find('.ignore').show();
-                        } else {
-                            // $notifs.find('.actions').hide();
-                        $notifs.find('.notif-text').text('You are now friends with @' + user.friend_username);
-                            $notifs.find('.accept').hide();
-                            $notifs.find('.ignore').hide();
-                        }
-                        
-                        $notifs.find('.friend_id').val(user.friend_id);
-                        $('.notifs-container').append($notifs);
-                    });
-                } else {
-                    alert(response.message);
-                }
-            },
-            error: function(xhr, status, error) {
-                console.error("AJAX request failed:", status, error);
-                alert("An error occurred while making the request.");
-            }
-        });
-    });
-
+    }
 });
-
-
